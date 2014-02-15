4418f4d359d1dce7f1f6c762a6037f09c6a39d70
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-15 12:09:50 -0500

    flask and vagrant junk

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install -y python
+sudo apt-get install -y python-pip
+sudo apt-get install -y vim
+sudo apt-get install -y python-mysqldb
+sudo pip install flask
+sudo pip install -r /vagrant/requirements.txt
+
+echo hello
