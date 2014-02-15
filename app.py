419da210ea141bf5aadf5acd3c2d61c195b7eacb
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-15 16:34:07 -0500

    results and such

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app.debug = True
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	return render_template("search.html")
+
+@app.route("/search")
+def search():
+	return render_template("search.html")
+
+@app.route("/results")
+def results():
+	return render_template("results.html")
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
