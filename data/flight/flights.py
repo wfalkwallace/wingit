f446e865ebac4cc13934b0d7e5db34b3888112a5
elemonier <emily.lemonier@gmail.com>
2014-02-15 12:12:35 -0500

    Adding fight schema

---
+++ data/flight/flights.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+flights = Blueprint('flight_blueprint', __name__)
+TABLE = 'flight_t'
+
+
+"""
+Dictionary of querystring params tied to their column names and
+a function to form a WHERE statement.
+"""
+
+flight_attributes = {
+
+	'IATA code': {
+		'column': '
+	
+	}
+}
