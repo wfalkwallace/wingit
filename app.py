75345cd189537884868fefc4fbd5591ed8154db9
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-15 17:16:23 -0500

    stuff

--- app.py
+++ app.py
@@ -7,13 +7,24 @@ app.debug = True
 def home():
 	return render_template("search.html")
 
-@app.route("/search")
+@app.route("/search", methods=["POST"])
 def search():
-	return render_template("search.html")
+	if request.method == "POST":
+		print 'signup-post'
+		return render_template("signup.html", signup_email=request.form["register_email"])
+	else: # request.method == "GET"
+		return render_template("search.html")
 
 @app.route("/results")
 def results():
 	return render_template("results.html")
 
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template("results.html"), 404
+
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
+
+
