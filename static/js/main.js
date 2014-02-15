1d88b0908a2578135fb5a0241753a79364cf6732
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-15 15:56:30 -0500

    ui stuff

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,16 @@
+$( document ).ready(function() {
 
+	window.setTimeout(function() {
+		$(".alert").fadeTo(500, 0).slideUp(500, function(){
+			$(this).remove(); 
+		});
+	}, 2000);
+
+	$( 'input[name="roundtrip"]' ).click(function() {
+		$("#returning-input").removeClass('hidden');
+	});
+	$( "#one-way-toggle" ).click(function() {
+		$("#returning-input").addClass('hidden');
+	});
+
+});
