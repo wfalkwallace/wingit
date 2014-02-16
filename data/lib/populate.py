c5335f19e7c8f92c8399c0a99a591646189292f0
elemonier <emily.lemonier@gmail.com>
2014-02-15 20:35:11 -0500

    flight updates.

--- data/lib/populate.py
+++ data/lib/populate.py
@@ -1,14 +1,29 @@
 import MySQLdb
 import datetime
+import sys
 
 #call: python populate.py <path_to_txt> <model>
+#path to airport codes: '../flight_codes.txt'
+#path to flight info: 
+
+text_name= sys.argv[0]
+model_type = sys.argv[1]
 
 # Open database connection
 db = MySQLdb.connect("wingit.crlh5pdu9hyk.us-east-1.rds.amazonaws.com",\
 	"root","wingitrealgood","wingitdb")
+
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
-txt_file = open('../flight_codes.txt', 'r')
+txt_file = open(, 'r')
+
+    # flight_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    # origin INT NOT NULL,
+    # dest INT NOT NULL,
+    # etd DATETIME NOT NULL,
+    # eta DATETIME NOT NULL,
+    # price DECIMAL NOT NULL,
+    # created_at DATETIME,
 
 for line in txt_file:
 	line_arr = line.rstrip().split(';')
@@ -17,14 +32,12 @@ for line in txt_file:
 	# Prepare SQL query to INSERT a record into the database.
 	#parse text file
 
-	a = line_arr[2]
-	b = line_arr[0]
-	c = line_arr[1]
-	d = str(datetime.datetime.now())
-	print a, ' , ', b,' , ', c,' , ', d
-	sql = "INSERT INTO airports(code, city, country, created_at) VALUES ('%s', '%s', '%s', '%s')" % \
-		( a, b, c, d )
-	
+	if model_type == 'airport':
+		sql = "INSERT INTO airports(code, city, country, created_at) VALUES ('%s', '%s', '%s', '%s')" % \
+			( line_arr[2], line_arr[0], line_arr[1], str(datetime.datetime.now()) )
+	if model_type == 'flight':
+		sql = "INSERT INTO flight(origin, dest, etd, eta, price, created_at) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % \
+			( line_arr[0], line_arr[1], line_arr[2], line_arr[3], line_arr[4], str(datetime.datetime.now()) )
 	cursor.execute(sql)
 
 try:
